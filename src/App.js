dcc738b69e3a222d1473ebc50613e2220bdd7a49
Tali Ngatuvai <Tngatuvai85@gmail.com>
2020-07-22 00:23:40 -0600

    + useEffect, API call to show list of countries in drop down menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([
+    'USA', 'UK', 'India',
+  ]);
+
+  // USEEFFECT = Runs a piece of code based on a given condition // 
+  useEffect(() => {
+    // The code inside here will will run once when the component loads and not again //
+    // async -> send a request, wait for it, do something with it
+
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2, 
+          }));
+    
+        setCountries(countries);
+      });
+    };
+
+    getCountriesData();
+  }, []);   
+
   return (
     <div className="App">
-      <h1>Lets build a Covid-19 tracker!!</h1>
-      
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries and show a drop down list of the options */}
+            
+            {
+              countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+
+              ))}
+
+          </Select>
+        </FormControl>
+      </div>
+
       {/* Header */}
       {/* Title + Select input dropdown field */}
-      
+
       {/* InfoBox */}
       {/* InfoBox */}
       {/* InfoBox */}
